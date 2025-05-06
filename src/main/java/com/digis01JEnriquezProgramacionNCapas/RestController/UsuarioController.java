7023508fc7cc3da488822107e43d5a8fbc7312b6
Jesús Adrian Sanchez <jasanrod00@gmail.com>
2025-05-05 18:39:19 -0600

    RestController

--- src/main/java/com/digis01JEnriquezProgramacionNCapas/RestController/UsuarioController.java
+++ src/main/java/com/digis01JEnriquezProgramacionNCapas/RestController/UsuarioController.java
@@ -14,6 +14,7 @@ import com.digis01JEnriquezProgramacionNCapas.JPA.Usuario;
 import com.digis01JEnriquezProgramacionNCapas.JPA.UsuarioDireccion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,9 +22,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
@@ -74,8 +75,8 @@ public class UsuarioController {
         }
     }
     
-    @GetMapping("GetById")
-    public ResponseEntity GetUsuarioById(@RequestParam int IdUsuario){
+    @GetMapping("GetById/{IdUsuario}")
+    public ResponseEntity GetUsuarioById(@PathVariable int IdUsuario){
         Result result = usuarioDAOImplementation.GetUsuarioById(IdUsuario);
         
         if(result.correct == true){
